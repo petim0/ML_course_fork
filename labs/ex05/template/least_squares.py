8d376c6ed3ad39f8ba5c380a94eab39c24d12550
Timo Achard <timo@apolline.eu>
2023-11-05 19:05:08 +0100

    did exo 5 and merged from upstream

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -5,13 +5,18 @@ Least Square
 """
 
 import numpy as np
-
+from costs import compute_mse
 
 def least_squares(y, tx):
     """calculate the least squares."""
     # ***************************************************
     # INSERT YOUR CODE HERE
     # least squares: TODO
+
+    w = np.linalg.solve(tx.T @ tx, tx.T @ y)
+    mse = compute_mse(y, tx, w)
+
+    return w, mse
+
     # returns mse, and optimal weights
     # ***************************************************
-    raise NotImplementedError
